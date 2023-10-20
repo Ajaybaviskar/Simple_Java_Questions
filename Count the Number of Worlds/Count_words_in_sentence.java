36a5204cbdd1542bb7c1d43a9ae401b26d4bba68
ajay <ajaybaviskar830@gmail.com>
2023-10-20 15:39:22 +0530

    inprove Count words in sencentece file

--- Count the Number of Worlds/Count_words_in_sentence.java
+++ Count the Number of Worlds/Count_words_in_sentence.java
@@ -5,13 +5,30 @@ import java.io.InputStreamReader;
 public class Count_words_in_sentence {
     public static void main(String args[]) throws IOException {
         BufferedReader Br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.print("Enter the String :--> "); // Tack input from user
+        int c = 0;
+        System.out.print("\n\tEnter the String :--> "); // Tack input from user
         String sentence = Br.readLine();
 
         // Display the number of worlds in sentence and paragraph
         int count = sentence.length();
-        System.out.println("Number of Worlds : " + count);
+        System.out.println("\n\tNumber of Worlds and Special characters are : " + count);
+
+        for (int i = 0; i < sentence.length(); i++) {
+            if (!Character.isDigit(sentence.charAt(i)) && !Character.isLetter(sentence.charAt(i))
+                    && !Character.isWhitespace(sentence.charAt(i)))
+            // check condition for digit,letter, and white spaces
+            {
+                // counter Increment by 1
+                c++;
+
+            }
+
+        }
+        if (c == 0) {
+            System.out.println("\n\tNO Special characters Founds ");
+        } else {
+            System.out.println("\n\tSpeical Characters " + c);
+        }
 
     }
 }
